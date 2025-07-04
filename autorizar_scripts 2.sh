b081737f1265e79547e7a57ea4360cb168d5d06f
Héctor Aguila <he.aguila@duocuc.cl>
2025-07-04 02:26:48 -0400

    Se borran algunos archivos duplicados

--- autorizar_scripts 2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# Script para autorizar (dar permisos de ejecución) a todos los scripts de gestión
-# Uso: ./autorizar_scripts.sh
-
-chmod +x run_all_services.sh restart_all_services.sh stop_all_services.sh autorizar_scripts.sh
-
-echo "Permisos de ejecución otorgados a los scripts: run_all_services.sh, restart_all_services.sh, stop_all_services.sh, autorizar_scripts.sh"
